8ae64f8cf7f1ba7bd0fd9575f0415a7e4f957181
ZapDos7 <ioanna.zapalidi@gmail.com>
2019-10-17 02:19:11 +0300

    fixed main, ebala ta arxeia gia tis g_funs kai ta hash tables alla den exoun kati tromero mesa tous & enhmerwsa ligo to readme.odt

--- ht.cpp
+++ ht.cpp
@@ -7,7 +7,7 @@ template <class T>
 ht<T>::~ht(){}
 
 template <class T>
-void ht<T>::set_vector(my_vector v) {}
+void ht<T>::set_vector(my_vector<T> v) {}
 
 template <class T>
-int ht<T>::get_vector_bucket_number(my_vector v) {}
+int ht<T>::get_vector_bucket_number(my_vector<T> v) {}
