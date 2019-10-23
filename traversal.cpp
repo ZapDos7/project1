f1436377d8a6f4dfed4dc8da1904f939083926a8
jimlaoshi <sdi1400006@di.uoa.gr>
2019-10-24 01:09:15 +0300

    traversal tree

--- traversal.cpp
+++ traversal.cpp
@@ -43,3 +43,67 @@ std::vector<std::pair<int, int>> traversal<T>::get_all_possible_travs()
 {
     return indices;
 }
+
+//TRAVERSAL TREE
+
+
+
+traversal_tree::traversal_tree(int curve1_length, int curve2_length){
+  traversal_node seed_of_life;
+  root = seed_of_life;
+  root.zeugos.first = 0;
+  root.zeugos.second = 0;
+  root.left = NULL;
+  root.center = NULL;
+  root.right = NULL;
+  root.recursive_builder(curve1_length, curve2_length);
+}
+
+void recursive_builder(int curve1_length, int curve2_length){
+  bool left_ok, center_ok, right_ok = true; //shmainei oti tha ginoyn anadromikes klhseis-dhmiourgia gia autes
+  //ypopshfio aristero paidi
+  left = new traversal_node();
+  left.zeugos.first = zeugos.first +1;
+  left.zeugos.second = zeugos.second;
+  left.left = NULL;
+  left.right = NULL;
+  left.center = NULL;
+
+  //ypopshfio kentriko paidi
+  center = new traversal_node();
+  center.zeugos.first = zeugos.first +1;
+  center.zeugos.second = zeugos.second+1;
+  center.left = NULL;
+  center.right = NULL;
+  center.center = NULL;
+
+  //ypopshfio deksio paidi
+  right = new traversal_node();
+  right.zeugos.first = zeugos.first;
+  right.zeugos.second = zeugos.second+1;
+  right.left = NULL;
+  right.right = NULL;
+  right.center = NULL;
+
+  if(zeugos.first == curve1_length-1) //ftasame sto telos ths 1hs kampylhs
+  {
+    delete left; left = NULL; left_ok = false;
+    delete center; center = NULL; center_ok = false;
+  }
+
+  if(zeugos.second == curve2_length-1) //ftasame sto telos ths 1hs kampylhs
+  {
+    delete right; right = NULL; right_ok = false;
+    delete center; center = NULL; center_ok = false;
+  }
+
+  if(left_ok)
+    left.recursive_builder(curve1_length, curve2_length);
+
+  if(center_ok)
+    center.recursive_builder(curve1_length, curve2_length);
+
+  if(right_ok)
+    right.recursive_builder(curve1_length, curve2_length);
+
+}
--- traversal.h
+++ traversal.h
@@ -11,9 +11,33 @@ private:
     <std::vector<std::pair<int, int>>> all_possible_travs;
 
 public:
-    traversal(curve<T> *c1, curve<T> *c2);
-    ~traversal();
+    traversal<T>(curve<T> *c1, curve<T> *c2);
+    ~traversal<T>();
     std::vector<std::pair<int, int>> get_all_possible_travs();
 };
+
+
+class traversal_node{
+public:
+  std::pair<int, int> zeugos;
+  traversal_node * left; //aristera shmainei kineitai o first
+  traversal_node * center; //kentro shmainei kinountai kai oi 2
+  traversal_node * right; //deksia shmainei kineitai o second
+
+  traversal_node(){};
+  ~traversal_node(){}; //DOULITSA
+  void recursive_builder(int curve1_length, int curve2_length); //sunexizei to dentro me riza ton idio ton komvo
+};
+
+class traversal_tree{
+private:
+  traversal_node root; //h riza tou dentrou me ta traversals
+public:
+  traversal_tree(int curve1_length, int curve2_length);
+  ~traversal_tree(){}; //DOULITSA
+
+
+};
+
 
 #endif
