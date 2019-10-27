a3dd78f6417cb859522b13bcd13157000682ffbc
jimlaoshi <sdi1400006@di.uoa.gr>
2019-10-28 00:21:39 +0200

    curves hypercube start

---
+++ curve_cube_ht.cpp
@@ -0,0 +1,191 @@
+#include "curve_cube_ht.h"
+#include <string>
+#include <random> // uniform_real_distribution
+
+template <class T>
+void cube_ht<T>::hamming_magic(std::string str, int i, int changesLeft)
+{
+  if (changesLeft == 0)
+  {
+    //printf("%s\n", str.c_str());
+
+    if(std::find(verticizer.begin(), verticizer.end(), str) != verticizer.end()) {
+    /* v contains x */
+    } else {
+    verticizer.push_back(str);
+    }
+    //verticizerx.insert(str);
+  }
+  if (i < 0)
+    return;
+  // flip current bit
+  str[i] = str[i] == '0' ? '1' : '0';
+  hamming_magic(str, i - 1, changesLeft - 1);
+  // or don't flip it (flip it again to undo)
+  str[i] = str[i] == '0' ? '1' : '0';
+  hamming_magic(str, i - 1, changesLeft);
+}
+
+template <class T>
+cube_ht<T>::cube_ht(int d_ton, int vector_dimensions, double w_to_be)
+{
+  d_tonos = d_ton;
+  size = (int)floor(pow(2, (double)d_ton));
+  cube.clear();
+  cube.resize(size);
+  for (int i = 0; i < size; i++)
+  {
+    cube[i].clear();
+  }
+
+  fi_seeds.clear();
+
+   std::random_device rand_dev;
+   std::mt19937 generator(rand_dev());
+   std::uniform_real_distribution<double>  distr(0, w_to_be);
+  for (int i = 0; i < d_ton; i++)
+  {
+
+    double gasmas = distr(generator);
+    gasmas = floor(gasmas);
+    fi_seeds.push_back((int)gasmas); //pairnei times o pinakas
+  }
+
+  my_gs.clear();
+  for (int i = 0; i < d_ton; i++)
+  {
+    g_funs<T> a_g_fun(k_ys, vector_dimensions, w_to_be);
+    my_gs.push_back(a_g_fun);
+  }
+
+  verticizer.clear();
+}
+
+template <class T>
+cube_ht<T>::~cube_ht() {}
+
+template <class T>
+int cube_ht<T>::fi_function(long int gval, int index)
+{
+  //long int mix = gval + fi_seeds[index];
+  long int mix = (gval + fi_seeds[index])/10;
+  //int fi = our_mod(mix, 2); //isws thelei %
+  int fi = mix % 2;
+  //std::cout << gval << " " << fi_seeds[index] << "\n";
+  return fi;
+}
+
+template <class T>
+void cube_ht<T>::cubify_vector(my_vector<T> *v)
+{
+  /*std::cout << "etsi hto o pinax prin hasharw to " << v->get_id_as_int() <<":";
+    for(int i =0; i< table.size(); i++)
+      for(int j =0; j< table[i].size(); j++)
+        std::cout << table[i][j].first->get_id_as_int();
+    std::cout << "\n";*/
+
+
+  std::string vertex = ""; //arxika adeio bitstring, einai h korufh tou yperkuvou sthn opoia tha brethei
+  for (unsigned int i = 0; i < my_gs.size(); i++)
+  {                                                  //gia kathe gi
+    long int gival = my_gs[i].actual_g_function(*v); //ypologise thn timh ths gi gia to vector v
+    int fi = fi_function(gival, i);                  //perase thn apo thn fi wste na pareis 0 h 1
+    std::string fi_str = std::to_string(fi);
+    vertex += fi_str; //concatenate to apotelesma sto teliko bitstring - korufh yperkuvou
+    //std::cout << fi;
+  }
+  unsigned long long key_vertex = std::stoull(vertex, NULL, 2); //metafrazei to bitstring-korufh yperkuvou se thesh pinaka
+
+  //std::pair<my_vector<T> * , long int> thepair;
+  //thepair.first = v;
+  //thepair.second = keyv;
+  cube[key_vertex].push_back(v); //apo8hkeuse to deikth sto dianusma
+                                 //std::cout << "my id is " << thepair.first->get_id_as_int() << "\n";
+  //std::cout << "inputvec  " << v->get_id() << " sto bucket " <<  key_vertex << '\n';
+}
+
+
+
+
+template <class T>
+std::vector<int> cube_ht<T>::cubify_query(my_vector<T> *q, int probes)
+{
+
+  int check_probes = probes;
+  std::string vertex = "";
+  for (unsigned int i = 0; i < my_gs.size(); i++)
+  {
+    long int gival = my_gs[i].actual_g_function(*q); //ypologise thn timh ths gi gia to query vector q
+    int fi = fi_function(gival, i);
+    std::string fi_str = std::to_string(fi);
+    vertex += fi_str;
+  }
+  unsigned long long key_vertex = std::stoull(vertex, NULL, 2); //metafrazei to bitstring-korufh yperkuvou se thesh pinaka
+  std::string matroska = "";
+  //std::cout << "QUERYtvec  " << q->get_id() << " sto bucket " <<  key_vertex << '\n';
+  matroska = std::to_string(key_vertex) + " ";
+
+  std::vector<int> this_q_potential_neighbs;
+  this_q_potential_neighbs.clear();
+  //GIA AUTO TO BUCKET/KORUFH
+  for (unsigned int i = 0; i < cube[key_vertex].size(); i++)
+  {
+    //std::cout << table[modded_keyv][i].first->get_id_as_int() << "\n";
+    this_q_potential_neighbs.push_back(cube[key_vertex][i]->get_id_as_int()); //valto sth lista pithanwn geitonwn
+                                                                              //std::cout << "my id is " << table[modded_keyv][i].first->get_id_as_int() << "\n";
+  }
+  check_probes--; //sto eclass eipw8hke oti sta probes sumperilamvanetai kai h idia h korufh
+  verticizer.clear();
+  std::string tempver = vertex;
+  size_t len = vertex.length();
+  size_t maxHammingDistance = len;
+  for (size_t i = 1 ; i <= maxHammingDistance ; ++i) {
+                //printf("Computing for distance %d from %s\n", i, vertex.c_str());
+                hamming_magic(tempver, len-1, i);
+                //printf("----------------\n");
+  }
+  //std::vector<std::string> verticizer(verticizerx.begin(), verticizerx.end());
+
+  //if (check_probes > d_tonos - 1) //den yparxoyn panw apo d_tonos-1 korufes gia na koitaksei
+    //check_probes = d_tonos - 1;
+  if (check_probes <= 0){
+    //std::cout << "QUERYtvec  " << q->get_id() << " sto bucket " <<  matroska << '\n';
+    return this_q_potential_neighbs;
+  }
+
+
+  //o verticizer periexei ta bitstrings twn korufwn poy mporoyn na koitaxtoun -alla de tha koitaxtoyn oles-
+  unsigned long long vert_key;
+  //std::vector<std::string> verticizer(verticizerx.begin(), verticizerx.end());
+  for (unsigned int i = 0; i < verticizer.size(); i++)
+  {
+    if (check_probes <= 0){
+      //std::cout << "QUERYtvec  " << q->get_id() << " sto bucket " <<  matroska << '\n';
+      return this_q_potential_neighbs;
+    }
+
+    vert_key = std::stoull(verticizer[i], NULL, 2); //metafrazei to bitstring-korufh yperkuvou se thesh pinaka
+    matroska += std::to_string(vert_key) + " ";
+    //std::cout << verticizer[i] << "\n";
+    for (unsigned int j = 0; j < cube[vert_key].size(); j++)
+    {
+      //std::cout << table[modded_keyv][i].first->get_id_as_int() << "\n";
+      this_q_potential_neighbs.push_back(cube[vert_key][j]->get_id_as_int()); //valto sth lista pithanwn geitonwn
+                                                                              //std::cout << "my id is " << table[modded_keyv][i].first->get_id_as_int() << "\n";
+    }
+    check_probes--;
+  }
+
+  /*std::cout << "brhka tous: ";
+    std::cout << "oi ypopshfioi geitonoi moy einai : ";
+    for(unsigned int yod = 0; yod < this_HT_potential_neighbs.size(); yod++)
+      std::cout << this_HT_potential_neighbs[yod];
+    std::cout << "\n";*/
+  verticizer.clear();
+  //std::cout << "QUERYtvec  " << q->get_id() << " sto bucket " <<  matroska << '\n';
+  return this_q_potential_neighbs;
+}
+
+//template class cube_ht<float>;
+template class cube_ht<int>;
+//template class cube_ht<double>;
---
+++ curve_cube_ht.h
@@ -0,0 +1,48 @@
+#ifndef CURVE_CUBE_HT_H_
+#define CURVE_CUBE_HT_H_
+
+#include "g_funs.h"
+#include <unordered_map>
+#include "my_vector.h"
+#include <fstream>
+#include <algorithm>
+#include "curve.h"
+
+
+template <class T>
+class cube_ht
+{
+private:
+  int size;
+  int k_ys = 3; //arithmos h sunarthsewn, prota8hke hardcoded sto eclass
+  int d_tonos;
+  //std::unordered_map<int, ht_cell<T> > table; //key == int, mapped value == my_vectors //https://en.cppreference.com/w/cpp/container/unordered_map
+  //std::vector<std::vector<std::pair<my_vector<T> *, long int>>> cube; //o "pinakas" opoy kathe bucket einai mia korufh tou yperkybou
+  std::vector<std::vector<curve<T> *>> cube; //o "pinakas" opoy kathe bucket einai mia korufh tou yperkybou kai exei mesa deikth sto vector
+  std::vector< g_funs<T> > my_gs; //oi sunarthseis g gia ton kybo
+  std::vector<int> fi_seeds; //prokyptei apo uniform kai krata enan tetoio arithmo gia kathe f poy tha "dhmiourghsoyme"
+
+  std::vector<std::string> verticizer;
+  //std::set<std::string> verticizerx;
+
+public:
+  //int size;
+  //std::vector< std::vector< std::pair<my_vector<T> * , long int> > > table; //pinakas (o hash table) apo pinakes (h lista ana bucket) apo zeugaria (zeugari dianusmatos kai timhs g)
+  //g_funs<T> my_g;
+  cube_ht<T>(int d_ton, int vector_dimensions, double w_to_be); //constructor
+  ~cube_ht();
+  int fi_function(long int gval, int index);
+  void cubify_vector(my_vector<T> *v, curve<T> *);
+  std::vector<int> cubify_query(my_vector<T> *q, int probes);
+  //int get_vector_bucket_number(my_vector v);
+  void hamming_magic(std::string str, int i, int changesLeft);
+};
+
+//supplementary function
+//void hamming_magic(std::string str, int i, int changesLeft);
+//boh8htikh metavlhth
+//std::vector<std::string> verticizer;
+
+
+
+#endif
